001bf0f8ade7dfd2ccef4011353cb39266381a3b
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-02-21 16:42:21 +0100

    up test_ihm

--- test_jb/test_comp/CompareMusic.h
+++ test_jb/test_comp/CompareMusic.h
@@ -2,8 +2,10 @@
 #define COMPARE_MUSIC_H
 
 #include <vector>
+#include <String.h>
 
 bool compareMelodies(const std::vector<float>& capturedNotes, const std::vector<float>& storedMelody);
 String findMatchingMelody(const std::vector<float>& capturedNotes);
+float calculateCorrelation(const std::vector<float>& a, const std::vector<float>& b);
 
 #endif // COMPARE_MUSIC_H
