7aa4475c316c3c221e4bfaf86d1feaf45ce4e2dc
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-02-21 16:45:06 +0100

    up ihm

--- test_jb/test_IHM/IHM.cpp
+++ test_jb/test_IHM/IHM.cpp
@@ -7,12 +7,14 @@ extern char filename[32];
 void handleCommand() {
   if (Serial.available()) {
     char command = Serial.read();
+
     if (command == 's') { // start capture
       // Commencer la capture des notes
       capturing = true;
       capturedNotes.clear();
       captureTimer.begin(captureNote, 10 * 1000); // 10 ms
       Serial.println("Capture commencée.");
+      
     } else if (command == 'e') {  // end capture
       // Arrêter la capture des notes
       capturing = false;
@@ -23,12 +25,14 @@ void handleCommand() {
       for (float note : notes) {
         Serial.println(note);
       }
+
     } else if (command == 'w') {  // write melody to SD
       Serial.println("Entrez le nom du fichier (avec extension .csv) :");
       while (!Serial.available()) {}
       Serial.readBytesUntil('\n', filename, sizeof(filename));
       filename[sizeof(filename) - 1] = '\0';
       storeMelody(capturedNotes, filename);
+
     } else if (command == 'r') { // read melody from SD
       Serial.println("Entrez le nom du fichier (avec extension .csv) :");
       while (!Serial.available()) {}
@@ -38,21 +42,22 @@ void handleCommand() {
       for (float note : melody) {
         Serial.println(note);
       }
+
     } else if (command == 'l') { // list stored melodies
       std::vector<String> melodies = getStoredMelodies();
       for (String melody : melodies) {
         Serial.println(melody);
       }
+
     } else if (command == 'c') { // compare music
-      Serial.println("Entrez le nom du fichier cible (avec extension .csv) :");
       while (!Serial.available()) {}
       Serial.readBytesUntil('\n', filename, sizeof(filename));
       filename[sizeof(filename) - 1] = '\0';
       String bestMatch = findMatchingMelody(loadMelody(filename));
       Serial.print("La mélodie la plus similaire est : ");
       Serial.println(bestMatch);
+      
     } else if (command == 'd') { // delete file
-      Serial.println("Entrez le nom du fichier à supprimer (avec extension .csv) :");
       while (!Serial.available()) {}
       Serial.readBytesUntil('\n', filename, sizeof(filename));
       filename[sizeof(filename) - 1] = '\0';
